a9ccc5b0d0dd26626a027edf0c75ac09d2d27804
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2007-07-26 09:52:51 +0000

    Backport the improvements of iqt and anynumpy from PyQwt-5.0.1.

--- qt3lib/Qwt4/anynumpy.py
+++ qt3lib/Qwt4/anynumpy.py
@@ -1,4 +1,4 @@
-# import either scipy, or numarray, or Numeric into numpy
+# import either NumPy, or numarray, or Numeric
 
 for name in ('numpy', 'numarray', 'Numeric'):
     failed = False
@@ -6,14 +6,31 @@ for name in ('numpy', 'numarray', 'Numeric'):
          eval(compile('from %s import *' % name, 'eval', 'exec'))
 	 if name == 'numpy':
 	     from numpy.oldnumeric.compat import *
-	     Float = float
-	     UInt8 = uint8
+             Float = float
+             UInt8 = uint8
     except ImportError:
         failed = True
     if not failed:
         break
 else:
-    raise SystemExit, 'Failed to import either scipy, or numarray, or Numeric'
+    import qt
+    # Provoke a runtime error when no QApplication instance exists, since
+    # qt.qApp does not return None when no QApplication instance exists.
+    try:
+        qt.qApp.name()
+    except RuntimeError:
+        a = qt.QApplication([])
+    qt.QMessageBox.critical(
+        None,
+        'Numerical Python Extension Required',
+        'This example requires a Numerical Python Extension, but\n'
+        'failed to import either NumPy, or numarray, or Numeric.\n'
+        'NumPy is available at http://sourceforge.net/projects/numpy'
+        )
+    raise SystemExit, (
+        'Failed to import either NumPy, or numarray, or Numeric.\n'
+        'NumPy is available at http://sourceforge.net/projects/numpy'
+        )
 
 # Local Variables: ***
 # mode: python ***
--- qt3lib/Qwt4/iqt.py
+++ qt3lib/Qwt4/iqt.py
@@ -3,17 +3,23 @@
 Provides control over PyQt and PyQwt widgets from the command line interpreter.
 """
 
+# Import GNU readline, so that readline can do its work in Python scripts.
+# _iqt falls back on a different method when there is no GNU readline.
 try:
-    import readline as _r
-    del _r
+    import readline
 except ImportError:
     pass
 
-from qt import QApplication
-_a = QApplication([])
+from qt import QApplication, qApp
+
+# Provoke a runtime error when no QApplication instance exists, since
+# qApp does not return None when no QApplication instance exists.
+try:
+    qApp.name()
+except RuntimeError:
+    _a = QApplication([])
 
 import _iqt 
-_iqt.hook(True)
 
 # Local Variables: ***
 # mode: python ***
